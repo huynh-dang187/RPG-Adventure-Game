7ecbb4b8c344e014c4a6d2a2a7bd412c05f518c8
huynh-dang187 <112720090+huynh-dang187@users.noreply.github.com>
2025-10-28 22:32:24 +0700

    Update Assets Bow and Its position

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 22f;
+
+    private void Update()
+    {
+        MoveProjectile();
+    }
+
+    private void MoveProjectile()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+    }
+}
--- Assets/Scripts/UI/Staff.cs
+++ Assets/Scripts/UI/Staff.cs
@@ -14,18 +14,14 @@ public class Staff : MonoBehaviour, IWeapon
     {
         Debug.Log("Staff Attack");
     }
-    
-    public WeaponInfo GetWeaponInfo()
-    {
-        return weaponInfo;
-    }
+
     private void MouseFollowWithOffset()
     {
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(PlayerController.Instance.transform.position);
 
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        if(mousePos.x < playerScreenPoint.x)
+        if (mousePos.x < playerScreenPoint.x)
         {
             ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, -180, angle);
 
@@ -34,5 +30,9 @@ public class Staff : MonoBehaviour, IWeapon
         {
             ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, angle);
         }
+    }
+    public WeaponInfo GetWeaponInfo()
+    {
+        return weaponInfo;
     }
 }

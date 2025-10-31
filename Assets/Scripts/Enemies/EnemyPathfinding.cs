88c2fac1f9fa7b539b946ce9a4ef11a93b1a0c12
huynh-dang187 <112720090+huynh-dang187@users.noreply.github.com>
2025-11-01 02:24:14 +0700

    Update Staff DealDamage

--- Assets/Scripts/Enemies/EnemyPathfinding.cs
+++ Assets/Scripts/Enemies/EnemyPathfinding.cs
@@ -9,16 +9,27 @@ public class EnemyPathfinding : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveDir;
     private Knockback Knockback;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         Knockback = GetComponent<Knockback>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() {
         if(Knockback.GettingKnockedBack) {return;}
-         
+
         rb.MovePosition(rb.position + moveDir * (moveSpeed * Time.fixedDeltaTime));
+        
+        if(moveDir.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipY = false; 
+        }
     }
 
     public void MoveTo(Vector2 targetPosition) {

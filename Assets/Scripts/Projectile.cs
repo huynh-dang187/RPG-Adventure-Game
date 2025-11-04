014c3466c35f431c3c4eecfdd359d8db52cb0f71
huynh-dang187 <112720090+huynh-dang187@users.noreply.github.com>
2025-11-05 01:14:29 +0700

    Update bullet flight path

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -25,6 +25,11 @@ public class Projectile : MonoBehaviour
         this.projectileRange = projectileRange;
     }
 
+    public void UpdateMoveSpeed(float moveSpeed)
+    {
+        this.moveSpeed = moveSpeed;
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
         Indestructible indestructible = other.gameObject.GetComponent<Indestructible>();
